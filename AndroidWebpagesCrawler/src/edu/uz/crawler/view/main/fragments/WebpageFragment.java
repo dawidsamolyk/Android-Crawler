cb6b440b4d43d2acdb4f9daa85c7ca302f491572
Dawid Samołyk <dawid.samolyk@gmail.com>
2015-06-19 21:45:06 +0200

    Resolved #25 : Show progress of webpage loading (on webpage navigator tab)

--- AndroidWebpagesCrawler/src/edu/uz/crawler/view/main/fragments/WebpageFragment.java
+++ AndroidWebpagesCrawler/src/edu/uz/crawler/view/main/fragments/WebpageFragment.java
@@ -5,9 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import edu.uz.crawler.R;
 import edu.uz.crawler.view.main.fragments.navigator.WebpagesNavigatorHandler;
 
@@ -15,24 +17,34 @@ public class WebpageFragment extends Fragment {
 	private WebView webpageView;
 
 	@Override
-	public View onCreateView(final LayoutInflater inflater,
-			final ViewGroup container, final Bundle savedInstanceState) {
+	public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
 
-		final View rootView = inflater.inflate(R.layout.fragment_webpage,
-				container, false);
+		final View rootView = inflater.inflate(R.layout.fragment_webpage, container, false);
 
 		webpageView = (WebView) rootView.findViewById(R.id.webpageView);
-		final EditText webpageAddress = (EditText) rootView
-				.findViewById(R.id.webpageAddressInput);
-		final Button backButton = (Button) rootView
-				.findViewById(R.id.backButton);
+		final EditText webpageAddress = (EditText) rootView.findViewById(R.id.webpageAddressInput);
+		final Button backButton = (Button) rootView.findViewById(R.id.backButton);
 
-		WebpagesNavigatorHandler webpagesNavigatorHandler = new WebpagesNavigatorHandler(
-				webpageView);
+		WebpagesNavigatorHandler webpagesNavigatorHandler = new WebpagesNavigatorHandler(webpageView);
 
 		webpageView.setWebViewClient(webpagesNavigatorHandler);
 		webpageAddress.setOnKeyListener(webpagesNavigatorHandler);
 		backButton.setOnClickListener(webpagesNavigatorHandler);
+
+		final ProgressBar progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
+
+		webpageView.setWebChromeClient(new WebChromeClient() {
+			@Override
+			public void onProgressChanged(WebView view, int progress) {
+				if (progress < 100 && progressBar.getVisibility() == ProgressBar.GONE) {
+					progressBar.setVisibility(ProgressBar.VISIBLE);
+				}
+				progressBar.setProgress(progress);
+				if (progress == 100) {
+					progressBar.setVisibility(ProgressBar.GONE);
+				}
+			}
+		});
 
 		return rootView;
 	}
